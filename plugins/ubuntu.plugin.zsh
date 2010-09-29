d8a44ad4fe3f1d90775bc195211ea36d30a57a76
newcity <newcity@staging.insidenewcity.com>
2010-09-29 18:31:12 +0000

    Added Directory assitance to Ubuntu plugin.

--- plugins/ubuntu.plugin.zsh
+++ plugins/ubuntu.plugin.zsh
@@ -4,3 +4,6 @@ alias update="sudo aptitude update"
 alias install="sudo aptitude install"
 alias upgrade="sudo aptitude safe-upgrade"
 alias remove="sudo aptitude remove"
+
+# Directory Assistance
+alias cda="cd /etc/apache2/sites-available/"
